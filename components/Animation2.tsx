65a5878c2260e1c992ec4020a61c658d197b88ad
thaoleem <thaolm@classfunc.com>
2024-12-04 13:35:44 +0700

    add input

--- components/Animation2.tsx
+++ components/Animation2.tsx
@@ -1,42 +1,32 @@
 "use client"
 import React, {useState} from 'react';
-import { useRive, useStateMachineInput } from '@rive-app/react-canvas';
+import {useRive, useStateMachineInput} from '@rive-app/react-canvas';
 
 export const Animation2 = () => {
-    const [input, setInput] = useState(0)
-    const [playing, setPlaying] = useState<boolean>(false)
-    console.log(input)
-    const { rive, RiveComponent } = useRive({
+    const [input, setInput] = useState<string>('default')
+    const {rive, RiveComponent} = useRive({
         src: '/rive/bingo.riv',
         stateMachines: "StateMachine1",
-        autoplay:false,
+        autoplay: false,
         artboard: 'mouth',
-        onStateChange:((event)=>{
-        })
     });
-    const bumpInput = useStateMachineInput(rive, "StateMachine1", "Number1");
-
     return (
-        <RiveComponent
-            className={`w-full h-[50vh]`}
-            onClick={() => {
-                if(rive){
-                    bumpInput && bumpInput.fire()
-                    console.log(rive.contents)
-                    // rive.play('StateMachine1', true)
-                    // rive.setNumberStateAtPath('Number1',input,'mouth')
-                    // console.log(rive.contents.artboards)
-                    // console.log(rive.playingStateMachineNames)
-                    setInput(prevState => prevState+1)
-                    // rive.setNumberStateAtPath()
-                }
-                // if(playing){
-                //     rive?.pause()
-                // }else {
-                //     rive?.play()
-                // }
-                // setPlaying(!playing)
-            }}
-        />
+        <div className={`w-full h-[50vh] flex flex-col justify-center items-center`}>
+            <div className={`w-full  flex gap-2 justify-center items-center`}>
+                <input className={`border w-3/4 rounded-md p-2`} value={input} onChange={(event) => {
+                    setInput(e => event.target.value)
+                }}/>
+                <button className={`bg-gray-400 p-2 rounded-md`}
+                        onClick={() => {
+                            if(rive){
+                                rive.play(`${input}`)
+                        }}}>
+                    Play
+                </button>
+            </div>
+            <RiveComponent
+                className={`w-full h-[50vh]`}
+            />
+        </div>
     );
 }
