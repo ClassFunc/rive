b37673b718d771fb9882660dfd3443d826243888
thaoleem <thaolm@classfunc.com>
2024-12-05 11:33:37 +0700

    update

--- components/Animation5.tsx
+++ components/Animation5.tsx
@@ -1,30 +1,48 @@
 "use client"
-import React, { useState, useEffect, useRef } from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import {useRive, useStateMachineInput} from '@rive-app/react-canvas';
 
 export const Animation5 = () => {
     const [number, setNumber] = useState(0);
-    const {rive, RiveComponent} = useRive({
+    const {rive: riveMain, RiveComponent} = useRive({
         src: '/rive/bingo.riv',
         artboard: 'Main',
-        stateMachines: "State_Machine_1",
+        stateMachines: ["State_Machine_1"],
         autoplay: true,
     });
-    const rating = useStateMachineInput(rive, "State_Machine_1", "Number_A");
+    const smMain = useStateMachineInput(riveMain, "State_Machine_1", "Number_A");
+
+    const {rive: riveMouth, RiveComponent: RiveComponent2} = useRive({
+        src: '/rive/bingo.riv',
+        artboard: 'mouth',
+        stateMachines: ["State_Machine_1"],
+        autoplay: true,
+    });
+    const smMouth = useStateMachineInput(riveMouth, "State_Machine_1", "Number_B");
 
 
     useEffect(() => {
-        if(rive && rating){
-            rating.value=number
-            console.log(rive.contents)
+        console.log({riveMain, smMain})
+        if (riveMain && smMain) {
+            smMain.value = number
         }
-    }, [number]);
-    
+    }, [number, riveMain]);
+
+    useEffect(() => {
+        console.log({riveMouth, smMouth})
+        if (riveMouth && smMouth) {
+            smMouth.value = number
+        }
+    }, [number, riveMouth]);
+
 
     return (
         <div className={`w-full h-[50vh] flex flex-col justify-center items-center`}>
-            <input type={'number'} min={0} max={6} value={number} onChange={(e) => setNumber(Number(e.target.value))}/>
+            <input type={'number'} min={0} max={20} value={number} onChange={(e) => setNumber(Number(e.target.value))}/>
             <RiveComponent
+                className={`w-full h-[50vh]`}
+            />
+            <RiveComponent2
                 className={`w-full h-[50vh]`}
             />
         </div>
